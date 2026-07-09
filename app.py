282e299a4ac99c75ba7071373f0a65c631dbc264
raphaelprahtamaid-source <raphaelprahtama.id@gmail.com>
2026-07-09 17:59:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,11 @@ def get_card(card_id):
 
     return {"error": "Card not found"}, 404
 
+import os
+
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
